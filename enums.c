a2f154dfcc24522b20fc2ac4e1c92666f46c08fd
FernandoOmen <fer_bsof@outlook.com>
2020-08-09 16:40:06 -0500

    Adding multiple values to multiple variables (enums)

--- enums.c
+++ enums.c
@@ -3,10 +3,23 @@
 
 enum months{January, February, March, April, May, June, July, August, September, October, November, December};
 
+enum army 
+{
+    ak47 = 120,
+    ak117 = 100,
+    m16 = 130,
+    msmc = 80,
+
+    
+} guns;
+
 int main(int argc, char const *argv[])
 {
-    enum months xmas;
+/*     enum months xmas;
     xmas = December;
-    printf("XMAS is the %dth month.", xmas+1);
+    printf("XMAS is the %dth month.", xmas+1); */
+
+    guns = m16;
+    printf("The damage of m16 is %d", sizeof(guns));
     return 0;
 }
