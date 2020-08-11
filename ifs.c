28eaf4b25e9a0379564a7edd8dc48bdbafcf6bad
FernandoOmen <fer_bsof@outlook.com>
2020-08-11 14:39:14 -0500

    Comparing values using if, else if and else

--- ifs.c
+++ ifs.c
@@ -1,16 +1,27 @@
 #include<stdio.h>
 
-if (/* condition */)
+int main(int argc, char const *argv[])
 {
-    // Statment
-    // Statment
+    int number_1;
+    int number_2;
+    printf("Enter number 1: ");
+    scanf("%d", &number_1);
+    printf("Enter number 2: ");
+    scanf("%d", &number_2);
+
+    if ( number_1 > number_2)
+        printf("The number 1: %d is bigger than the number 2: %d", number_1,  number_2);
+    else if (number_1 < number_2)
+        printf("The number 1: %d is smaller than the number 2: %d", number_1,  number_2);
+    else
+        printf("The number 1: %d is equal to the number 2: %d", number_1,  number_2);
+
+    
+
+
+
+    return 0;
 }
-else if (/* condition */)
-    // Statment
-else
-{
-    // Statment
-    // Statment
-}
+
 
 
