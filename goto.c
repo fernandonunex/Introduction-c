8e7c36d9a553bf5d2127261ddb4e9f4279a5f243
FernandoOmen <fer_bsof@outlook.com>
2020-08-15 14:28:19 -0500

    It´s a schema of how work goto in C.

---
+++ goto.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    for (size_t i = 0; i < count; i++)
+    {
+        for (size_t i = 0; i < count; i++)
+        {
+            for (size_t i = 0; i < count; i++)
+            {
+                goto: errorHandler
+            }
+            
+        }
+        
+    }
+    
+errorHandeler: 
+// All the code to reset the program
+    return 0;
+}
