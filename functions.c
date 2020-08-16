0ac87e84f1a2b3b2d0ed555abe285fcbc36c2f6c
FernandoOmen <fer_bsof@outlook.com>
2020-08-16 13:03:03 -0500

    Creating functions in C

---
+++ functions.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int squared(int n, int m);
+
+
+int main(int argc, char const *argv[])
+{
+    int i;
+    for ( i = 0; i < 10; i++)
+    {
+        printf("n = %d The result is: %d \n",i, squared(2,i));
+    }
+
+    printf("The result is: %d \n",squared(5,2));
+
+    return 0;
+}
+
+
+int squared(int n, int m)
+{
+    int i, p;
+    p = 1;
+    for ( i = 1; i <= m; i++)
+    {
+        p = p * n;
+    }
+    return p;
+}
+
--- goto.c
+++ goto.c
@@ -2,13 +2,14 @@
 
 int main(int argc, char const *argv[])
 {
+    int count;
     for (size_t i = 0; i < count; i++)
     {
         for (size_t i = 0; i < count; i++)
         {
             for (size_t i = 0; i < count; i++)
             {
-                goto: errorHandler
+                // goto: errorHandler;
             }
             
         }
